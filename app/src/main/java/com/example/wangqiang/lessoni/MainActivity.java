d98e5b250648b2c1258a28a027fcefe226e8d4e2
wangqiang0208 <wangqiang0208@gmail.com>
2015-03-01 16:27:33 +0800

    first commit

--- app/src/main/java/com/example/wangqiang/lessoni/MainActivity.java
+++ app/src/main/java/com/example/wangqiang/lessoni/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView3 = (TextView) findViewById(R.id.TextView3);
+        Button button = (Button) findViewById(R.id.Button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView3.setText("You clicked up");
+            }
+        });
     }
 
 
